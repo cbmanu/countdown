7475c34d684fca90b22e805783defada8b61d960
cbmanu <carrero.besson@gmail.com>
2021-11-01 14:30:20 -0400

    Countdown V1.0
    Todo funciona, falta agregarle mas funciones y optimizar

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-let myTime = document.querySelector('.timer');
-let restart = document.getElementById('restart')
-let start = document.querySelector('.start');
-let edit = document.getElementById('edit');
 let editTable = document.querySelector('.edit-table')
 let submit = document.querySelector('.submit')
-
+let myTime = document.querySelector('.timer');
+let restart = document.getElementById('restart')
+let start = document.getElementById('start');
+let edit = document.getElementById('edit');
+let end = document.getElementById('stop')
 let alarm = new Audio('sounds/381382__coltonmanz__alarm.wav')
-let timeE = 30;
-let timeR = 5;
 let minutesE;
 let secondsE;
 let minutesR;
 let secondsR;
 
 restart.addEventListener('click',()=>{
+    timeE = 0;
+    timeR = 0;
+    timeE =+ minutesE * 60 + secondsE;
+    timeR =+ minutesR * 60 + secondsR;
+    if(minutesE == undefined && secondsE == undefined){
+        timeE = 0;
+    }
+    if(minutesR == undefined && secondsR == undefined){
+        timeR = 0;
+    }
+    if (Math.floor(timeE/60) < 10 && Math.floor(timeE%60) <10) {
+        myTime.innerHTML = `0${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+        timeE--;
+        }
+        else if(Math.floor(timeE/60) < 10 && Math.floor(timeE%60) > 10){
+            myTime.innerHTML = `0${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else if(Math.floor(timeE/60) == 10 && Math.floor(timeE%60) < 10){
+            myTime.innerHTML = `${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else{
+            myTime.innerHTML = `${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
 
-    
 })
 
 function show(){
@@ -30,7 +53,22 @@ submit.addEventListener('click',()=>{
     checkSecondsR();
     timeE = minutesE * 60 + secondsE;
     timeR = minutesR * 60 + secondsR;
-    console.log(`${timeE},${timeR}`)
+    if (Math.floor(timeE/60) < 10 && Math.floor(timeE%60) <10) {
+        myTime.innerHTML = `0${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+        timeE--;
+        }
+        else if(Math.floor(timeE/60) < 10 && Math.floor(timeE%60) > 10){
+            myTime.innerHTML = `0${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else if(Math.floor(timeE/60) == 10 && Math.floor(timeE%60) < 10){
+            myTime.innerHTML = `${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else{
+            myTime.innerHTML = `${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
 })
 
 
@@ -38,58 +76,64 @@ edit.addEventListener('click',function(){
     show();
 });
 
-
-
-start.addEventListener('click',function(e){
-    start.classList.replace('start','stop');
-    let end = document.querySelector('.stop')
-    start.textContent = ''
-    start.textContent = 'Stop'
-    end.addEventListener('click',()=>{
-        e.stopPropagation()
-    })
-    setInterval(time,1000)
-            function time(){
-                if(timeE > 0 ){
-                    if (Math.floor(timeE/60) < 10 && Math.floor(timeE%60) <10) {
-                    myTime.innerHTML = `0${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
-                    timeE--;
-                    }
-                    else if(Math.floor(timeE/60) < 10 && Math.floor(timeE%60) > 10){
-                        myTime.innerHTML = `0${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
-                        timeE--;
-                    }
-                    else{
-                        myTime.innerHTML = `${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
-                        timeE--;
-                    }
-                }
-                else if(timeE == 0){
-                    alarm.play();
-                    timeE--
-                }
-                else if(timeE == -1 && timeR > 0){
-                    if (Math.floor(timeR/60) < 10 && Math.floor(timeR%60) <10) {
-                        myTime.innerHTML = `0${Math.floor(timeR/60)}:0${Math.floor(timeR%60)}`;
-                        timeR--;
-                        }
-                        else if(Math.floor(timeR/60) < 10 && Math.floor(timeR%60) > 10){
-                            myTime.innerHTML = `0${Math.floor(timeR/60)}:${Math.floor(timeR%60)}`;
-                            timeR--;
-                        }
-                        else{
-                            myTime.innerHTML = `${Math.floor(timeR/60)}:${Math.floor(timeR%60)}`;
-                            timeR--;
-                        }
-                }
-                else if(timeE == -1 && timeR == 0){
-                    alarm.play();
-                    timeE =+ minutesE * 60 + secondsE;
-                    timeR =+ minutesR * 60 + secondsR;
-                }
+function time(){
+    if(timeE > 0 ){
+        if (Math.floor(timeE/60) < 10 && Math.floor(timeE%60) <10) {
+        myTime.innerHTML = `0${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+        timeE--;
+        }
+        else if(Math.floor(timeE/60) < 10 && Math.floor(timeE%60) > 10){
+            myTime.innerHTML = `0${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else if(Math.floor(timeE/60) == 10 && Math.floor(timeE%60) < 10){
+            myTime.innerHTML = `${Math.floor(timeE/60)}:0${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+        else{
+            myTime.innerHTML = `${Math.floor(timeE/60)}:${Math.floor(timeE%60)}`;
+            timeE--;
+        }
+    }
+    else if(timeE == 0){
+        alarm.play();
+        timeE--
+    }
+    else if(timeE == -1 && timeR > 0){
+        if (Math.floor(timeR/60) < 10 && Math.floor(timeR%60) <10) {
+            myTime.innerHTML = `0${Math.floor(timeR/60)}:0${Math.floor(timeR%60)}`;
+            timeR--;
             }
-})
+            else if(Math.floor(timeR/60) < 10 && Math.floor(timeR%60) > 10){
+                myTime.innerHTML = `0${Math.floor(timeR/60)}:${Math.floor(timeR%60)}`;
+                timeR--;
+            }
+            else if(Math.floor(timeR/60) == 10 && Math.floor(timeR%60) < 10){
+                myTime.innerHTML = `${Math.floor(timeR/60)}:0${Math.floor(timeR%60)}`;
+                timeR--;
+            }
+            else{
+                myTime.innerHTML = `${Math.floor(timeR/60)}:${Math.floor(timeR%60)}`;
+                timeR--;
+            }
+    }
+    else if(timeE == -1 && timeR == 0){
+        alarm.play();
+        timeE =+ minutesE * 60 + secondsE;
+        timeR =+ minutesR * 60 + secondsR;
+    }
+}
 
+end.addEventListener('click',()=>{
+    clearInterval(tick)
+    end.style.display = 'none'
+    start.style.display = 'inline-block'
+    })
+start.addEventListener('click',function(){
+    tick = setInterval(time,1000)
+    start.style.display = 'none'
+    end.style.display = 'inline-block'
+})
 
 
 
